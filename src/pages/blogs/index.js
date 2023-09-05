aab9c1f06fa639706c1d46fd716d93070dfacbc2
John <emailjohnthomascaballero@gmail.com>
2023-09-05 22:57:21 +0800

    boiler plate

---
+++ src/pages/blogs/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from "src/components/Footer";
+import Navbar from "src/components/Navbar";
+
+const contact = () => {
+  return (
+    <div>
+      <Navbar />
+        <h1>blogs</h1>
+      <Footer />
+    </div>
+    
+  )
+}
+
+export default contact
---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import Footer from "src/components/Footer";
+import Navbar from "src/components/Navbar";
+
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      <Navbar />
+        <h1>contact</h1>
+      <Footer />
+    </div>
+  )
+}
+
+export default contact
