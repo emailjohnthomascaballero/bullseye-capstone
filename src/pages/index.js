a3c831dcbfabe2ab95f4b68be59b4584c8d51b10
John <emailjohnthomascaballero@gmail.com>
2023-09-06 15:12:22 +0800

    feat(navbar): completed the navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import { Oswald } from "next/font/google";
-import Footer from "src/components/Footer";
-import Navbar from "src/components/Navbar";
+import Footer from "src/components/footer";
+import Navbar from "src/components/navbar";
 
 const oswald = Oswald({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className={`text-3xl ${oswald.className}`}>
+    <div className={`${oswald.className}`}>
       <Navbar />
 
-      <h1>Hello World</h1>
+      <h1>Hello World</h1> 
 
       <Footer />
     </div>
