eb7c46cf5216755a1f3435373239231289f986df
John <emailjohnthomascaballero@gmail.com>
2023-09-10 16:33:34 +0800

    feat(blogscontent): completed section

--- src/pages/blogs/blogscontent.js
+++ src/pages/blogs/blogscontent.js
@@ -1,41 +1,154 @@
-import React from 'react'
+import React from "react";
 
 const posts = [
   {
     id: 1,
-    title: 'Boost your conversion rate',
-    href: '#',
+    title: "Expert Advice, How to Choose the Right Gun for Your…",
+    href: "#",
     description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    imageUrl:
-      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    imageUrl: "/photos/blog-expert-advice.jpg",
+    iconDateUrl: "/icons/date.svg",
+    date: "May 15, 2023",
+    datetime: "2020-03-16",
+    iconCategoryUrl: "/icons/category.svg",
+    // category: { title: "Tips & Tricks", href: "#" },
+    category: "Tips & Tricks",
     author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "#",
       imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 1,
+    title: "5 Essential Tips for Taking Your Shooting Skills from Good…",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    imageUrl: "/photos/blog-essential-tips.jpg",
+    iconDateUrl: "/icons/date.svg",
+    date: "May 15, 2023",
+    datetime: "2020-03-16",
+    iconCategoryUrl: "/icons/category.svg",
+    // category: { title: "Tips & Tricks", href: "#" },
+    category: "Tips & Tricks",
+    author: {
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 1,
+    title: "The History of Shooting Sports, From Ancient Times to the…",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    imageUrl: "/photos/blog-history-shooting.jpg",
+    iconDateUrl: "/icons/date.svg",
+    date: "May 15, 2023",
+    datetime: "2020-03-16",
+    iconCategoryUrl: "/icons/category.svg",
+    // category: { title: "Tips & Tricks", href: "#" },
+    category: "Tips & Tricks",
+    author: {
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 1,
+    title: "Guns, Gear, and Good Times, Behind the Scenes at Bullseye…",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    imageUrl: "/photos/blog-guns-gear.jpg",
+    iconDateUrl: "/icons/date.svg",
+    date: "May 15, 2023",
+    datetime: "2020-03-16",
+    iconCategoryUrl: "/icons/category.svg",
+    // category: { title: "Tips & Tricks", href: "#" },
+    category: "Tips & Tricks",
+    author: {
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 1,
+    title: "The Top 10 Reasons Why Shooting Sports Are Great for…",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    imageUrl: "/photos/blog-top-10.jpg",
+    iconDateUrl: "/icons/date.svg",
+    date: "May 15, 2023",
+    datetime: "2020-03-16",
+    iconCategoryUrl: "/icons/category.svg",
+    // category: { title: "Tips & Tricks", href: "#" },
+    category: "Tips & Tricks",
+    author: {
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 1,
+    title: "How Our Shooting Club Can Help You Take Your Skills…",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    imageUrl: "/photos/blog-our-shooting.jpg",
+    iconDateUrl: "/icons/date.svg",
+    date: "May 15, 2023",
+    datetime: "2020-03-16",
+    iconCategoryUrl: "/icons/category.svg",
+    // category: { title: "Tips & Tricks", href: "#" },
+    category: "Tips & Tricks",
+    author: {
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
   },
   // More posts...
-]
+];
 
 const blogscontent = () => {
   return (
-    <div className="bg-customColor2 py-24 sm:py-32">
+    <div className="bg-customPrimary py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">From the blog</h2>
-          <p className="mt-2 text-lg leading-8 text-gray-600">
-            Learn how to grow your business with our expert advice.
-          </p>
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <div className="flex items-center mx-auto max-w-2xl lg:mx-0">
+            <img src="/icons/vertical-bar.svg" className="mr-1 h-16 lg:h-20" />
+            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-5xl uppercase">
+              YOU NEED TO KNOW WITH <br /> OUR LATEST BLOG
+            </h2>
+          </div>
         </div>
+
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
-            <article key={post.id} className="flex flex-col items-start justify-between">
+            <article
+              key={post.id}
+              className="flex flex-col items-start justify-between"
+            >
               <div className="relative w-full">
                 <img
                   src={post.imageUrl}
@@ -45,37 +158,33 @@ const blogscontent = () => {
                 <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
               </div>
               <div className="max-w-xl">
-                <div className="mt-8 flex items-center gap-x-4 text-xs">
-                  <time dateTime={post.datetime} className="text-gray-500">
+                <div className="mt-8 flex items-center gap-x-4 text-base">
+                  <img
+                    className=" h-6 w-6 md:h-7 md:w-7"
+                    src={post.iconDateUrl}
+                    alt=""
+                  />
+                  <time dateTime={post.datetime} className="text-customColor1">
                     {post.date}
                   </time>
-                  <a
-                    href={post.category.href}
-                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                  >
-                    {post.category.title}
-                  </a>
+
+                  <img
+                    className=" h-6 w-6 md:h-7 md:w-7"
+                    src={post.iconCategoryUrl}
+                    alt=""
+                  />
+                  <time dateTime={post.datetime} className="text-customColor1">
+                    {post.category}
+                  </time>
                 </div>
+
                 <div className="group relative">
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                  <h3 className="mt-3 text-2xl font-bold leading-6 text-white hover:text-gray-400 duration-500">
                     <a href={post.href}>
                       <span className="absolute inset-0" />
                       {post.title}
                     </a>
                   </h3>
-                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
-                </div>
-                <div className="relative mt-8 flex items-center gap-x-4">
-                  <img src={post.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-100" />
-                  <div className="text-sm leading-6">
-                    <p className="font-semibold text-gray-900">
-                      <a href={post.author.href}>
-                        <span className="absolute inset-0" />
-                        {post.author.name}
-                      </a>
-                    </p>
-                    <p className="text-gray-600">{post.author.role}</p>
-                  </div>
                 </div>
               </div>
             </article>
@@ -83,7 +192,7 @@ const blogscontent = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default blogscontent
+export default blogscontent;
