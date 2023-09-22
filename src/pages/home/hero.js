3e0adef16f54d7281697451c7e0233487ff1a655
John <emailjohnthomascaballero@gmail.com>
2023-09-23 00:01:47 +0800

    fix(hero): reduce bandwith usage

--- src/pages/home/hero.js
+++ src/pages/home/hero.js
@@ -1,23 +1,29 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const heroSection = () => {
   return (
     <div className="pt-28 lg:pt-14 xl:pt-24">
       <div className="relative bg-customColor2">
         <div className="relative h-80 overflow-hidden bg-customColor2 md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2">
-          <img
+          <Image
             className="floatElement h-full w-full object-cover"
             src="/photos/hero-banner.png"
             alt="hero banner"
+            width="1057"
+            height="915"
           />
         </div>
         <div className="relative mx-auto max-w-7xl py-14 sm:py-32 lg:px-8 lg:py-52">
           <div className="pl-6 pr-6 md:ml-auto md:w-2/3 md:pl-16 lg:w-1/2 lg:pl-24 lg:pr-0 xl:pl-32">
             <div className="flex items-center">
-              <img
+              <Image
                 src="/icons/vertical-bar.svg"
                 className="mr-1 h-24 md:h-52 lg:h-80"
+                alt="vertical icon"
+                width="24"
+                height="320"
               />
               <h2 className="text-5xl md:text-8xl font-bold text-white order-2">
                 FROM NOVICE
@@ -36,7 +42,7 @@ const heroSection = () => {
                 className="inline-flex bg-customSecondary px-3.5 py-2.5 text-sm font-semibold text-white uppercase shadow-sm hover:bg-white/20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white duration-500"
               >
                 Explore More{" "}
-                <img src="/icons/arrow-right-white.svg" className="ml-2" />
+                <Image src="/icons/arrow-right-white.svg" className="ml-2" alt="arrow point right" width="16" height="20" />
               </Link>
             </div>
           </div>
