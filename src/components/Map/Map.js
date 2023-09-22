32d1b9305dc1970a96069e6976acf61eb84bc94f
John <emailjohnthomascaballero@gmail.com>
2023-09-22 18:16:51 +0800

    feat(map): Add popup to map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,7 @@
 import "leaflet/dist/leaflet.css";
 import style from "../../styles/Home.module.css";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Popup } from "react-leaflet";
 
 function Map() {
   const markerPosition = [7.065292635066598, 125.60850799636214];
@@ -10,13 +10,15 @@ function Map() {
     <MapContainer
       className={style.map}
       center={markerPosition}
-      zoom={20}
+      zoom={13}
       scrollWheelZoom={false}
     >
       <TileLayer
         attribution="bullseye map"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
+      <Popup position={markerPosition}>{"Here I am"}</Popup>
     </MapContainer>
   );
 }
