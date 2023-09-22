6c1c92415578c7e7f7a5ff93484f0335cda05143
John <emailjohnthomascaballero@gmail.com>
2023-09-22 23:01:21 +0800

    fix(navbar): reduce bandwith usage

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
+import Link from "next/link";
 
 const navigation = [
   { name: "News", href: "#" },
@@ -18,7 +19,7 @@ export default function Navbar() {
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
       >
-        <a href="#" className="-m-1.5 p-1.5">
+        <Link href="/" className="-m-1.5 p-1.5">
           <span className="sr-only">Your Company</span>
           <Image
             src="/logos/logo.svg"
@@ -26,7 +27,7 @@ export default function Navbar() {
             width="226"
             height="71"
           />
-        </a>
+        </Link>
         <div className="flex lg:hidden">
           <button
             type="button"
@@ -38,20 +39,20 @@ export default function Navbar() {
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
-          <a
+          <Link
             href="/"
             className="text-xl uppercase font-bold leading-6 text-customColor1 hover:text-gray-300 transition duration-500"
           >
             Home
-          </a>
+          </Link>
           {navigation.map((item) => (
-            <a
+            <Link
               key={item.name}
               href={item.href}
               className="text-xl uppercase font-bold leading-6 text-white hover:text-gray-400 transition duration-500"
             >
               {item.name}
-            </a>
+            </Link>
           ))}
         </div>
       </nav>
@@ -62,9 +63,9 @@ export default function Navbar() {
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-customPrimary px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-300">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-20 w-full overflow-y-auto bg-customPrimary px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-300">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link href="/" >
               <span className="sr-only">Your Company</span>
               <Image
                 src="/logos/logo.svg"
@@ -72,7 +73,7 @@ export default function Navbar() {
                 width="226"
                 height="71"
               />
-            </a>
+            </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-white hover:text-gray-400 duration-500"
@@ -85,20 +86,20 @@ export default function Navbar() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <a
-                  href="#"
+                <Link
+                  href="/"
                   className="-mx-3 block rounded-lg px-3 py-2 text-xl uppercase font-bold leading-7 text-customColor1 hover:bg-gray-800 hover:text-gray-300 duration-500"
                 >
                   Home
-                </a>
+                </Link>
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-xl uppercase font-bold leading-7 text-white hover:bg-gray-800 hover:text-gray-400 duration-500"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
             </div>
