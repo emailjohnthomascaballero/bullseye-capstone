dcb8d1e1155398650f989c0b1c8a14eaa3de9b70
John <emailjohnthomascaballero@gmail.com>
2023-09-23 20:25:24 +0800

    fix(safefunaffordable): Add &apos; in paragraph

--- src/pages/home/safefunaffordable.js
+++ src/pages/home/safefunaffordable.js
@@ -76,7 +76,7 @@ const safefunaffordable = () => {
             </div>
 
             <p className="mt-12 mb-16 text-base leading-8 text-customColor1">
-              Whether you're a seasoned marksman or a first-time shooter, our
+              Whether you&apos;re a seasoned marksman or a first-time shooter, our
               facilities and services are designed to ensure your time with us
               is safe, enjoyable, and budget-friendly. Join us and be part of a
               community that values and promotes accessible shooting for all.
