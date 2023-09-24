44ead45949707e18a81e3f8800f7dd7eb7bee42c
John <emailjohnthomascaballero@gmail.com>
2023-09-24 13:10:55 +0800

    fix(_app): remove  Synchronous scripts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,11 @@
 import "@/styles/globals.css";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
-      </Head>
       <Component {...pageProps} />
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
     </>
   );
 }
