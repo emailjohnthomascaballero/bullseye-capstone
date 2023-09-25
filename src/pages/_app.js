ef80b3a988e205e9d1186f6c0d7bd6f2a47d02df
John <emailjohnthomascaballero@gmail.com>
2023-09-25 15:00:02 +0800

    fix(_app): Load scripts asynchronously

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js" strategy="beforeInteractive" />
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js" strategy="beforeInteractive" />
     </>
   );
 }
